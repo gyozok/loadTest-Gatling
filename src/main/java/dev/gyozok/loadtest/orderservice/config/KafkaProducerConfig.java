ad3331b288756959973e4d04dcfd4bfc273fd575
gyozok <karer.gyozo@gmail.com>
2026-09-23 06:31:12 +0200

    fix typo

--- src/main/java/dev/gyozok/loadtest/orderservice/config/KafkaProducerConfig.java
+++ src/main/java/dev/gyozok/loadtest/orderservice/config/KafkaProducerConfig.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
-    @Value("${spring.kafka.bootstrap-server}")
+    @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServer;
 
     @Bean
